67d6c4927f6cb76b79013cec4e956f5d1a6c734e
quocuong <cuong.hq.2235@aptechlearning.edu.vn>
2024-03-28 00:42:34 +0700

    Tao phan payment va shipping

---
+++ Art_Gallery/Art_Gallery/Controllers/PaymentController.cs
@@ -0,0 +1,55 @@
+﻿using Art_Gallery.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Art_Gallery.Controllers
+{
+    public class PaymentController : BaseController
+    {
+        private Art_GalleryEntities db = new Art_GalleryEntities();
+        // GET: Payment
+        public ActionResult Index(int orderId)
+        {
+            var payment = db.Purcher_order
+                .SingleOrDefault(a => a.OrderId == orderId);
+
+            var paymentMethods = new List<SelectListItem>
+            {
+                new SelectListItem { Value = "Cash", Text = "Cash" },
+            };
+
+            ViewBag.PaymentMethods = paymentMethods;
+            ViewBag.ArtworkName = payment.Artwork.Name;
+            ViewBag.TotalAmount = payment.TotalAmount;
+            return View(payment);
+        }
+
+        // POST: Payment/Create
+        [HttpPost]
+        public async Task<ActionResult> Create([Bind(Include = "PaymentId,PaymentMethod,Amount,PaymentDate,CustomerId,OrderId,PhoneNumber,Address")] Payment_gateways payment_gateways)
+        {
+            try
+            {
+                Purcher_order order = await db.Purcher_order.FindAsync(payment_gateways.OrderId);
+                if (order != null)
+                {
+                    order.StatusCode = "I";
+                }
+                payment_gateways.StatusCode = "A";
+                payment_gateways.PaymentDate = DateTime.Now;
+                db.Payment_gateways.Add(payment_gateways);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index", "OrderCustomer");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
